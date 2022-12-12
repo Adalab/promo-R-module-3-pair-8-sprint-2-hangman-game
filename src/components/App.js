bf04337a458c03e2cf397c9610d890b510714b36
lauragonzalezcalvo <laura24gc@gmail.com>
2022-12-12 11:21:09 +0100

    Add the propTypes

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,27 @@
 // import monigota from '../images/monigota.png';
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import '../styles/App.scss';
-import callToApi from '../services/api';
-import Header from './Header';
-import SolutionLetters from './SolutionLetters';
-import ErrorLetters from './ErrorLetters';
-import Dummy from './Dummy';
-import Form from './Form';
-import Instructions from './Instructions';
-import Options from './Options';
-import Footer from './Footer';
-import Loading from './Loading';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import "../styles/App.scss";
+import callToApi from "../services/api";
+import Header from "./Header";
+import SolutionLetters from "./SolutionLetters";
+import ErrorLetters from "./ErrorLetters";
+import Dummy from "./Dummy";
+import Form from "./Form";
+import Instructions from "./Instructions";
+import Options from "./Options";
+import Footer from "./Footer";
+import Loading from "./Loading";
 
 function App() {
   // VARIABLES ESTADO
   // Varible estado para incrementar el número de fallos
   // const [numberOfErrors, setNumberOfErrors] = useState(0);
   // Variable estado para guardar el carácter introducido en el input
-  const [lastLetter, setLastLetter] = useState('');
+  const [lastLetter, setLastLetter] = useState("");
   // Variable estado para almacenar la palabra que se deberá adivinar.
-  const [word, setWord] = useState('katakroker');
+  const [word, setWord] = useState("katakroker");
   // Variable estado para para almacenar y pintar las letras que introduce la jugadora.
   const [userLetters, setUserLetters] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -66,12 +66,12 @@ function App() {
       // ponemos entre corchetes por que es un array sino no lo identificaría como un string lo que cambia en el valor de la constante
       setUserLetters([...userLetters, value]);
     } else {
-      setLastLetter('');
+      setLastLetter("");
     }
   };
 
   const handleChange = (value) => {
-    setLastLetter('');
+    setLastLetter("");
     setUserLetters([]);
     setWord(value);
   };
@@ -79,14 +79,14 @@ function App() {
   // Función para pintar las letras, pero no nos pinta las mayusculas buenas
 
   const renderSolutionLetters = () => {
-    const wordLetters = word.split('');
+    const wordLetters = word.split("");
     return wordLetters.map((eachLetter, index) => {
       const exist = userLetters.includes(eachLetter.toLowerCase());
       //Gracias a key podemos ver los guiones sin que se vean las letras.
       // Hemos hecho un ternario para pintar las letras si coinciden con la letra de la Api
       return (
-        <li key={index} className='letter'>
-          {exist ? eachLetter : ''}
+        <li key={index} className="letter">
+          {exist ? eachLetter : ""}
         </li>
       );
     });
@@ -101,7 +101,7 @@ function App() {
     );
     return errorLetters.map((letter, index) => {
       return (
-        <li key={index} className='letter'>
+        <li key={index} className="letter">
           {letter}
         </li>
       );
@@ -109,15 +109,15 @@ function App() {
   };
 
   return (
-    <div className='App'>
-      <div className='page'>
+    <div className="App">
+      <div className="page">
         <Header />
 
-        <main className='main'>
+        <main className="main">
           <Loading isLoading={isLoading} />
           <Routes>
             <Route
-              path='/'
+              path="/"
               element={
                 <section>
                   <SolutionLetters
@@ -137,9 +137,9 @@ function App() {
                 </section>
               }
             />
-            <Route path='/instructions' element={<Instructions />} />
+            <Route path="/instructions" element={<Instructions />} />
             <Route
-              path='/options'
+              path="/options"
               element={<Options handleChange={handleChange} word={word} />}
             />
           </Routes>
--- src/components/ErrorLetters.js
+++ src/components/ErrorLetters.js
@@ -1,12 +1,16 @@
-import '../styles/components/Letters.scss';
+import "../styles/components/Letters.scss";
+import PropTypes from "prop-types";
 
 function ErrorLetters(props) {
   return (
-    <div className='error'>
-      <h2 className='title'>Letras falladas:</h2>
-      <ul className='letters'>{props.renderErrorLetters()}</ul>
+    <div className="error">
+      <h2 className="title">Letras falladas:</h2>
+      <ul className="letters">{props.renderErrorLetters()}</ul>
     </div>
   );
 }
+ErrorLetters.propTypes = {
+  renderErrorLetters: PropTypes.func.isRequired,
+};
 
 export default ErrorLetters;
